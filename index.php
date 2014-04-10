0cd34652ccfa29e9e9ae0e85f6b686036608b1fb
amosnsengi <amosnsengi20082@yahoo.com>
2014-04-09 20:31:31 -0400

    First version

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Widget Code</title>
+</head>
+
+<body>
+	<header>
+		<h1>Welcome to our site!</h1>
+        <nav>
+        	<a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+        </nav>
+    </header>
+    
+    <section>
+    	<h2>Intro</h2>
+        <p>lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette lipsum href tot fin ans te ma tette
+        </p>
+    </section>
+    
+    <footer>
+    	<p>Widget Code &copy;2014</p>
+    </footer>
+</body>
+</html>
