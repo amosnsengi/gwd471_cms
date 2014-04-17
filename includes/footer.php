49e0c9f3b62a03e79c40b56ce45f70206a64c94f
amosnsengi11 <amosnsengi20082@yahoo.com>
2014-04-17 00:27:12 +0000

    added css, js, includes, folders

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer>
+    	<p>Widget Code &copy;2014</p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+		<h1>Welcome to our site!</h1>
+        <nav>
+        	<a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+        </nav>
+ </header>
